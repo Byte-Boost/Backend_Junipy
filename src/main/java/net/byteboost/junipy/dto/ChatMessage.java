0905d585dbde59de354053a998cfbb5279073aa7
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2025-09-26 19:56:20 -0300

    [feat]: get ai responses from ai service

--- src/main/java/net/byteboost/junipy/dto/ChatMessage.java
+++ src/main/java/net/byteboost/junipy/dto/ChatMessage.java
@@ -3,14 +3,17 @@ package net.byteboost.junipy.dto;
 public class ChatMessage {
     private String role;
     private String message;
+    private String error;
 
     public ChatMessage() {}
 
-    public ChatMessage(String role, String message) {
+    public ChatMessage(String role, String message, String error) {
         this.role = role;
         this.message = message;
+        this.error = error;
     }
-
+    public String getError() { return error; }
+    public void setError(String error) { this.error = error; }
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
     public String getMessage() { return message; }
